164306117a2891690b2403245165c4e7e7546cd4
Javi H. Gil <javihgil@softspring.es>
2026-02-25 08:37:43 +0100

    Fix code style

--- src/Controller/SectionController.php
+++ src/Controller/SectionController.php
@@ -12,7 +12,6 @@ use Softspring\CmsSectionsPlugin\Model\SectionVersionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Translation\LocaleSwitcher;
 use Twig\Environment;
 
 class SectionController extends AbstractController
