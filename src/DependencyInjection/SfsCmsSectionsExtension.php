caf5d783b91df5979567d4698fe56f1e39eed7ba
Javi H. Gil <javihgil@softspring.es>
2026-06-18 11:58:30 +0200

    Fix code style

--- src/DependencyInjection/SfsCmsSectionsExtension.php
+++ src/DependencyInjection/SfsCmsSectionsExtension.php
@@ -6,9 +6,9 @@ use Symfony\Component\AssetMapper\AssetMapperInterface;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class SfsCmsSectionsExtension extends Extension implements PrependExtensionInterface
 {
